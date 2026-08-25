b3ef146702b42bcf8533df09e895416e1028a011
shakeethmohammad24 <shakeethmohammad24@gmail.com>
2026-08-25 04:05:15 +0000

    fix: eks node group ami type and k8s version, trigger frontend CI

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -188,7 +188,7 @@ resource "aws_iam_role_policy_attachment" "eks_service" {
 ##################
 # Track latest release for the given k8s version
 data "aws_ssm_parameter" "eks_ami_release_version" {
-  name = "/aws/service/eks/optimized-ami/${aws_eks_cluster.main.version}/amazon-linux-2/recommended/release_version"
+  name = "/aws/service/eks/optimized-ami/${aws_eks_cluster.main.version}/amazon-linux-2023/x86_64/standard/recommended/release_version"
 }
 
 resource "aws_eks_node_group" "main" {
@@ -199,6 +199,7 @@ resource "aws_eks_node_group" "main" {
   subnet_ids      = [var.enable_private == true ? aws_subnet.private_subnet.id : aws_subnet.public_subnet.id]
   release_version = nonsensitive(data.aws_ssm_parameter.eks_ami_release_version.value)
   instance_types  = ["t3.small"]
+  ami_type       = "AL2023_x86_64_STANDARD"
 
   scaling_config {
     desired_size = 1
